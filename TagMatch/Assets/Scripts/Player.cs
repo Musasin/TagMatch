ac846e76a00e7e13f0f8069bc240a70a367a8c5c
Musasin <c0112051b9@edu.teu.ac.jp>
2020-10-01 22:50:39 +0900

    プレイヤー移動/ジャンプ

---
+++ TagMatch/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    const float MOVE_VELOCITY = 5.0f;
+    const float JUMP_VELOCITY = 16.0f;
+    float velocityX = 0, velocityY = 0;
+    Rigidbody2D rb;
+    bool isRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocityX = rb.velocity.x;
+        velocityY = rb.velocity.y;
+        UpdateMove();
+        UpdateJump();
+        rb.velocity = new Vector2(velocityX, velocityY);
+    }
+
+    private void UpdateMove()
+    {
+        float dx = Input.GetAxisRaw("Horizontal");
+        if (dx > 0)
+        {
+            velocityX = MOVE_VELOCITY;
+            isRight = true;
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (dx < 0)
+        {
+            velocityX = -MOVE_VELOCITY;
+            isRight = false;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            velocityX = 0;
+        }
+    }
+    private void UpdateJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocityY = JUMP_VELOCITY;
+        }
+    }
+}
