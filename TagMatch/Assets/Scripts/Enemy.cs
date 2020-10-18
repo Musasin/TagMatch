5d2994a3113c0501c9069d05cb135406887a8a57
Musasin <c0112051b9@edu.teu.ac.jp>
2020-10-18 21:32:51 +0900

    エネミーのサイズを変えられるように調整

--- TagMatch/Assets/Scripts/Enemy.cs
+++ TagMatch/Assets/Scripts/Enemy.cs
@@ -12,18 +12,20 @@ public class Enemy : MonoBehaviour
     public bool isRight;
 
     Rigidbody2D rb;
+    Vector2 defaultScale;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        defaultScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(velocityX * (isRight ? 1 : -1), rb.velocity.y);
-        transform.localScale = new Vector2(isRight ? -1 : 1, 1);
+        transform.localScale = new Vector2(defaultScale.x * (isRight ? -1 : 1), defaultScale.y); // デフォルトは左向き
     }
 
     public void HitWall()
