56d8f452d61f33390bb9ba01c6c5d2fcbea732e4
Musasin <c0112051b9@edu.teu.ac.jp>
2020-10-27 00:57:56 +0900

    敵との当たり判定をEnterからStayに切り替え

--- TagMatch/Assets/Scripts/Player.cs
+++ TagMatch/Assets/Scripts/Player.cs
@@ -266,7 +266,7 @@ public class Player : MonoBehaviour
     }
 
     
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (invincibleTime > 0)
         {
