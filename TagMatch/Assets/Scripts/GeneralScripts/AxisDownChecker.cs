45548ad4ff73476d6c2c6e80f55a8e66afbf4bf4
Musasin <c0112051b9@edu.teu.ac.jp>
2020-11-07 08:41:45 +0900

    スキルツリーをマスタ管理して読み込む仕組みを作成

---
+++ TagMatch/Assets/Scripts/GeneralScripts/AxisDownChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisDownChecker : MonoBehaviour
+{
+    static float beforeAxisVertical, beforeAxisHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void AxisDownUpdate()
+    {
+        beforeAxisHorizontal = Input.GetAxisRaw("Horizontal");
+        beforeAxisVertical = Input.GetAxisRaw("Vertical");
+    }
+    
+    public static bool GetAxisDownHorizontal()
+    {
+        return beforeAxisHorizontal == 0 && (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0);
+    }
+    public static bool GetAxisDownVertical()
+    {
+        return beforeAxisVertical == 0 && (Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0);
+    }
+}
---
+++ TagMatch/Assets/Scripts/UI/SkillSelect.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class SkillSelect : MonoBehaviour
+{
+    private char lf = (char)10;
+    TextAsset csvFile;
+    string nowKey;
+    Text skillNameText, skillDescriptionText;
+
+    private struct SkillTreeData
+    {
+        public string unique_key;
+        public string name;
+        public string description;
+        public int level;
+        public string release_condition;
+        public int pos_x;
+        public int pos_y;
+        public string r_key;
+        public string l_key;
+        public string u_key;
+        public string d_key;
+        public GameObject gameObject;
+
+        public SkillTreeData(string unique_key, string name, string description, string level, string release_condition, string pos_x, string pos_y, string r_key, string l_key, string u_key, string d_key)
+        {
+            this.unique_key = unique_key;
+            this.name = name;
+            this.description = description;
+            this.level = int.Parse(level);
+            this.release_condition = release_condition;
+            this.pos_x = int.Parse(pos_x);
+            this.pos_y = int.Parse(pos_y);
+            this.r_key = r_key;
+            this.l_key = l_key;
+            this.u_key = u_key;
+            this.d_key = d_key;
+            gameObject = GameObject.Find(unique_key);
+            if (this.release_condition == "")
+            {
+                gameObject.transform.Find("RockPanel").gameObject.SetActive(false);
+            }
+            Transform levelIcon1 = gameObject.transform.Find("LevelIcon1");
+            if (levelIcon1 != null && this.level != 1) levelIcon1.gameObject.SetActive(false);
+            Transform levelIcon2 = gameObject.transform.Find("LevelIcon2");
+            if (levelIcon2 != null && this.level != 2) levelIcon2.gameObject.SetActive(false);
+            Transform levelIcon3 = gameObject.transform.Find("LevelIcon3");
+            if (levelIcon3 != null && this.level != 3) levelIcon3.gameObject.SetActive(false);
+        }
+    }
+    private Dictionary<string, SkillTreeData> skillTrees = new Dictionary<string, SkillTreeData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skillNameText = GameObject.Find("SkillNameText").GetComponent<Text>();
+        skillDescriptionText = GameObject.Find("SkillDescriptionText").GetComponent<Text>();
+
+        csvFile = Resources.Load("MasterData/skill_tree") as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+
+        int i = 0;
+        while (reader.Peek() > -1)
+        {
+            i++;
+            string line = reader.ReadLine();
+            string[] datas = line.Split(',');
+            if (i == 1) // 一行目はカラム名のためskip
+            {
+                continue;
+            }
+            if (i == 2) // 初期化用に一番初めのキーを入れとく
+            {
+                nowKey = datas[0];
+            }
+            var skillData = new SkillTreeData(datas[0],datas[1],datas[2],datas[3],datas[4],datas[5],datas[6],datas[7],datas[8],datas[9],datas[10]);
+            Debug.Log(skillData);
+            skillTrees.Add(datas[0], skillData);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AxisDownChecker.GetAxisDownHorizontal())
+        {
+            if (Input.GetAxisRaw("Horizontal") < 0)
+                nowKey = skillTrees[nowKey].l_key;
+            else if (Input.GetAxisRaw("Horizontal") > 0)
+                nowKey = skillTrees[nowKey].r_key;
+        }
+
+        if (AxisDownChecker.GetAxisDownVertical())
+        {
+            if (Input.GetAxisRaw("Vertical") < 0)
+                nowKey = skillTrees[nowKey].d_key;
+            else if (Input.GetAxisRaw("Vertical") > 0)
+                nowKey = skillTrees[nowKey].u_key;
+        }
+        
+        Debug.Log(skillTrees[nowKey].pos_x);
+        Debug.Log(skillTrees[nowKey].pos_y);
+        transform.localPosition = new Vector2(skillTrees[nowKey].pos_x, skillTrees[nowKey].pos_y);
+        skillNameText.text = skillTrees[nowKey].name;
+        skillDescriptionText.text = skillTrees[nowKey].description.Replace("\\n", lf.ToString());
+        
+        AxisDownChecker.AxisDownUpdate();
+    }
+}
