8daf0cfd9a6e41758d1472b50c5f44e6113c8fa2
Musasin <c0112051b9@edu.teu.ac.jp>
2020-12-13 14:46:06 +0900

    Add AtomCraftProject

---
+++ AtomCraftProject/binary_copy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp -f ./Public/Assets/StreamingAssets/* ../TagMatch/Assets/StreamingAssets/
+cp -f ./Public/inGamePreview/Assets/StreamingAssets/* ../TagMatch/Assets/StreamingAssets/InGame/
