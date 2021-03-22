8ea58400a13d41df9808eaf7a6091002d5245c02
Musasin <c0112051b9@edu.teu.ac.jp>
2021-03-22 14:11:28 +0900

    タイトル画面の背景作成中

---
+++ TagMatch/Assets/Scripts/TitleCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCamera : MonoBehaviour
+{
+    const float CAMERA_MOVE_SPEED = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector2(transform.position.x + 100, transform.position.y), CAMERA_MOVE_SPEED * Time.deltaTime);
+    }
+}
