45912f26caddd65dde300c242c1004060c7adef5
Musasin <c0112051b9@edu.teu.ac.jp>
2021-10-10 04:31:50 +0900

    ステータス画面のテキスト更新処理作成

--- TagMatch/Assets/Scripts/UI/Menu.cs
+++ TagMatch/Assets/Scripts/UI/Menu.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField]
+    Text yukariStatusText, makiStatusText, yukariSkillText, makiSkillText;
+
     Animator anim;
     enum MenuState
     {
@@ -53,6 +57,7 @@ public class Menu : MonoBehaviour
                 break;
 
             case MenuState.MENU:
+                UpdateStatusText();
                 if (AxisDownChecker.GetAxisDownVertical())
                 {
                     AudioManager.Instance.PlaySE("select");
@@ -60,7 +65,7 @@ public class Menu : MonoBehaviour
                     else if (Input.GetAxisRaw("Vertical") > 0) nowSelection--;
                     if (nowSelection > MenuList.CLOSE) nowSelection = MenuList.STATUS;
                     if (nowSelection < MenuList.STATUS) nowSelection = MenuList.CLOSE;
-                    menuCursor.transform.localPosition = new Vector2(cussorDefaultPos.x, cussorDefaultPos.y - (50 * (int)nowSelection) - (nowSelection == MenuList.CLOSE ? 50 :0));
+                    menuCursor.transform.localPosition = new Vector2(cussorDefaultPos.x, cussorDefaultPos.y - (50 * (int)nowSelection) - (nowSelection == MenuList.CLOSE ? 50 : 0));
                 }
                 AxisDownChecker.AxisDownUpdate();
 
@@ -91,7 +96,7 @@ public class Menu : MonoBehaviour
                     anim.SetInteger("menuState", (int)menuState);
                 }
                 break;
-                
+
             case MenuState.STATUS:
                 if (KeyConfig.GetJumpKeyDown() || KeyConfig.GetShotKeyDown() || KeyConfig.GetMenuKeyDown())
                 {
@@ -100,7 +105,7 @@ public class Menu : MonoBehaviour
                     anim.SetInteger("menuState", (int)menuState);
                 }
                 break;
-                
+
             case MenuState.SKILL:
                 if (KeyConfig.GetShotKeyDown() || KeyConfig.GetMenuKeyDown())
                 {
@@ -124,6 +129,39 @@ public class Menu : MonoBehaviour
                 }
                 break;
         }
+    }
+
+    private void UpdateStatusText()
+    {
+        yukariStatusText.text = "　　　【STATUS】\n";
+        yukariStatusText.text += "HP: " + StaticValues.yukariHP + "/" + StaticValues.yukariMaxHP + "\n";
+        yukariStatusText.text += "MP: " + StaticValues.yukariMP + "/" + StaticValues.yukariMaxMP + "\n";
+        yukariStatusText.text += "武器: スターシューター\n";
+        yukariStatusText.text += "威力: " + (int)(10 * StaticValues.yukariAttackRatio) + "\n";
+        yukariStatusText.text += "連射数: 5\n";
+        
+        makiStatusText.text = "　　　【STATUS】\n";
+        makiStatusText.text += "HP: " + StaticValues.makiHP + "/" + StaticValues.makiMaxHP + "\n";
+        makiStatusText.text += "MP: " + StaticValues.makiMP + "/" + StaticValues.makiMaxMP + "\n";
+        makiStatusText.text += "武器: むすタン\n";
+        makiStatusText.text += "威力: " + (int)(15 * StaticValues.makiAttackRatio) + "\n";
+        makiStatusText.text += "連射数: 2\n";
+
+        yukariSkillText.text = "　　　【SKILL】\n";
+        if (StaticValues.GetSkill("y_dash_1")) yukariSkillText.text += "・空中ダッシュ\n";
+        if (StaticValues.GetSkill("y_dash_2")) yukariSkillText.text += "・ダッシュ中無敵\n";
+        if (StaticValues.GetSkill("y_shot_1")) yukariSkillText.text += "・対空射撃\n";
+        if (StaticValues.GetSkill("y_shot_2")) yukariSkillText.text += "・対地射撃\n";
+        if (StaticValues.GetSkill("y_down_1")) yukariSkillText.text += "・低姿勢射撃\n";
+        if (StaticValues.GetSkill("y_down_2")) yukariSkillText.text += "・精密射撃\n";
+        
+        makiSkillText.text = "　　　【SKILL】\n";
+        if (StaticValues.GetSkill("m_jump_1")) makiSkillText.text += "・二段ジャンプ\n";
+        if (StaticValues.GetSkill("m_jump_2")) makiSkillText.text += "・くるりんジャンプ\n";
+        if (StaticValues.GetSkill("m_shot_1")) makiSkillText.text += "・エレキバリヤー\n";
+        if (StaticValues.GetSkill("m_shot_2")) makiSkillText.text += "・グレート\n　エレキファイヤー\n";
+        if (StaticValues.GetSkill("m_down_1")) makiSkillText.text += "・ヒール\n";
+        if (StaticValues.GetSkill("m_down_2")) makiSkillText.text += "・かくれる\n";
     }
 
     private void CloseMenu()
