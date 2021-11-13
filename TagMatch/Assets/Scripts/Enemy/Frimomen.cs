61282c68cec717c61d106bac811102aeec5a8a6f
Musasin <c0112051b9@edu.teu.ac.jp>
2021-11-14 01:00:19 +0900

    フリモメンの攻撃パターン追加 エネルギー弾

--- TagMatch/Assets/Scripts/Enemy/Frimomen.cs
+++ TagMatch/Assets/Scripts/Enemy/Frimomen.cs
@@ -7,15 +7,18 @@ using DG.Tweening;
 public class Frimomen : BossAIBase
 {
     public bool isAstral;
-    public GameObject starBullet;
+    public GameObject throwBullet;
     BoxCollider2D bc;
-    GameObject bulletPivot, squatBulletPivot;
-    Vector2 l1Pos, l2Pos, r1Pos, r2Pos, jumpOutPos;
+    GameObject bulletPivot;
+    Vector2 l1Pos, l2Pos, l3Pos, r1Pos, r2Pos, r3Pos, c1Pos, jumpOutPos;
     Animator dangerPanel1, dangerPanel2, dangerPanel3;
 
     enum ActionState
     {
         START,
+        JUMP_OUT,
+        JUMP_TO_C1,
+        JUMP_TO_R1,
         WARP_TO_R1,
         WARP_TO_R2,
         WARP_TO_R3,
@@ -25,6 +28,7 @@ public class Frimomen : BossAIBase
         TUCKLE_TO_L1,
         TUCKLE_TO_L2,
         TUCKLE_TO_L3,
+        THROW_BULLET_1,
         CHANGE_IS_RIGHT,
         LOOP,
         WAIT,
@@ -43,8 +47,14 @@ public class Frimomen : BossAIBase
         bossScript = GetComponent<Boss>();
         state = ActionState.START;
         
+        bulletPivot = transform.Find("BulletPivot").gameObject;
         l1Pos = GameObject.Find("L1Pos").transform.position;
+        l2Pos = GameObject.Find("L2Pos").transform.position;
+        l3Pos = GameObject.Find("L3Pos").transform.position;
         r1Pos = GameObject.Find("R1Pos").transform.position;
+        r2Pos = GameObject.Find("R2Pos").transform.position;
+        r3Pos = GameObject.Find("R3Pos").transform.position;
+        c1Pos = GameObject.Find("C1Pos").transform.position;
         jumpOutPos = GameObject.Find("JumpOutPos").transform.position;
         dangerPanel1 = GameObject.Find("DangerPanel1").GetComponent<Animator>();
         dangerPanel2 = GameObject.Find("DangerPanel2").GetComponent<Animator>();
@@ -95,6 +105,8 @@ public class Frimomen : BossAIBase
             return;
         }
 
+        Debug.Log(state);
+
         switch (state)
         {
             case ActionState.START:
@@ -105,9 +117,27 @@ public class Frimomen : BossAIBase
                 
                 actionStateQueue.Add(ActionState.WAIT);
                 
+                //actionStateQueue.Add(ActionState.WARP_TO_R1);
+                //actionStateQueue.Add(ActionState.DANGER_1);
+                //actionStateQueue.Add(ActionState.TUCKLE_TO_L1);
+                
+                //actionStateQueue.Add(ActionState.WARP_TO_R2);
+                //actionStateQueue.Add(ActionState.DANGER_2);
+                //actionStateQueue.Add(ActionState.TUCKLE_TO_L2);
+                
+                //actionStateQueue.Add(ActionState.WARP_TO_R3);
+                //actionStateQueue.Add(ActionState.DANGER_3);
+                //actionStateQueue.Add(ActionState.TUCKLE_TO_L3);
+
                 actionStateQueue.Add(ActionState.WARP_TO_R1);
-                actionStateQueue.Add(ActionState.DANGER_1);
-                actionStateQueue.Add(ActionState.TUCKLE_TO_L1);
+                actionStateQueue.Add(ActionState.JUMP_TO_C1);
+                actionStateQueue.Add(ActionState.WAIT);
+                actionStateQueue.Add(ActionState.WAIT);
+                actionStateQueue.Add(ActionState.THROW_BULLET_1);
+                actionStateQueue.Add(ActionState.THROW_BULLET_1);
+                actionStateQueue.Add(ActionState.THROW_BULLET_1);
+                actionStateQueue.Add(ActionState.WAIT);
+                actionStateQueue.Add(ActionState.JUMP_TO_R1);
                 
                 actionStateQueue.Add(ActionState.LOOP);
 
@@ -116,17 +146,57 @@ public class Frimomen : BossAIBase
                 isPlaying = false;
                 stateIndex = 0;
                 break;
+            case ActionState.JUMP_OUT:
+                isPlaying = true;
+                PlayJumpSequence(jumpOutPos);
+                break;
+            case ActionState.JUMP_TO_C1:
+                isPlaying = true;
+                PlayJumpSequence(c1Pos);
+                break;
+            case ActionState.JUMP_TO_R1:
+                isPlaying = true;
+                PlayJumpSequence(r1Pos);
+                break;
             case ActionState.WARP_TO_R1:
                 isPlaying = false;
                 transform.position = r1Pos;
+                break;
+            case ActionState.WARP_TO_R2:
+                isPlaying = false;
+                transform.position = r2Pos;
+                break;
+            case ActionState.WARP_TO_R3:
+                isPlaying = false;
+                transform.position = r3Pos;
                 break;
             case ActionState.DANGER_1:
                 isPlaying = true;
                 PlayDangerSequence(dangerPanel1);
                 break;
+            case ActionState.DANGER_2:
+                isPlaying = true;
+                PlayDangerSequence(dangerPanel2);
+                break;
+            case ActionState.DANGER_3:
+                isPlaying = true;
+                PlayDangerSequence(dangerPanel3);
+                break;
             case ActionState.TUCKLE_TO_L1:
                 isPlaying = true;
                 PlayTuckleSequence(l1Pos);
+                break;
+            case ActionState.TUCKLE_TO_L2:
+                isPlaying = true;
+                PlayTuckleSequence(l2Pos);
+                break;
+            case ActionState.TUCKLE_TO_L3:
+                isPlaying = true;
+                PlayTuckleSequence(l3Pos);
+                break;
+            case ActionState.THROW_BULLET_1:
+                isPlaying = true;
+                PlayThrowSequence(throwBullet);
                 break;
             case ActionState.CHANGE_IS_RIGHT:
                 isRight = !isRight;
@@ -178,6 +248,39 @@ public class Frimomen : BossAIBase
             .Append(transform.DOLocalMove(targetPos, 1.0f)).SetEase(Ease.Linear)
             .OnComplete(() => { 
                 anim.SetBool("isTuckle", false);
+                isPlaying = false;
+            })
+            .Play();
+    }
+    private void PlayThrowSequence(GameObject bulletObj)
+    {
+        GameObject bullet = Instantiate(bulletObj);
+        bullet.SetActive(false);
+        bullet.transform.position = bulletPivot.transform.position;
+
+        float angleZ = Random.Range(90, 180);
+        float power = Random.Range(550, 800);
+        float addforceX = Mathf.Cos(angleZ * Mathf.Deg2Rad) * 550.0f;
+        float addforceY = Mathf.Sin(angleZ * Mathf.Deg2Rad) * 550.0f;
+
+        sequence = DOTween.Sequence()
+            .AppendCallback(() =>
+            {
+                anim.SetBool("isThrow", true);
+            })
+            .AppendInterval(0.1f)
+            .AppendCallback(() =>
+            {
+                bullet.SetActive(true);
+            })
+            .AppendInterval(0.3f)
+            .AppendCallback(() =>
+            {
+                bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(addforceX, addforceY));
+            })
+            .AppendInterval(1.0f)
+            .OnComplete(() => { 
+                anim.SetBool("isThrow", false);
                 isPlaying = false;
             })
             .Play();
