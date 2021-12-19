16e622b1dd6d59c927cbeb6454e395a0cb6962f1
Musasin <c0112051b9@edu.teu.ac.jp>
2021-12-19 18:10:03 +0900

    エンディングだいたい完成

---
+++ TagMatch/Assets/Scripts/Ending.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.LoadACB("Ending", "Ending.acb", "Ending.awb");
+        AudioManager.Instance.PlayBGM("ending");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
