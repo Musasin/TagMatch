37a73c669124a85d2daf94d7d13bd0a55f2b320b
Musasin <c0112051b9@edu.teu.ac.jp>
2021-12-26 05:28:13 +0900

    一個目のボイスがフェードイン中の暗い状態から再生されちゃうのを止めるため、強制実行の時は少し待ってから再生

--- TagMatch/Assets/Scripts/TalkFlag.cs
+++ TagMatch/Assets/Scripts/TalkFlag.cs
@@ -9,20 +9,33 @@ public class TalkFlag : MonoBehaviour
     Talk talkScript;
     bool isPlayed;
 
+    float time;
+    float delayTime = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         talkScript = GameObject.Find("TalkUI").GetComponent<Talk>();
         if (isForcePlay)
         {
-            talkScript.SetScenario(scenarioFileName, false);
-            Destroy(gameObject);
+            // 発火前に動かれないように止めておく
+            StaticValues.isTalkPause = true;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isForcePlay)
+        {
+            // 強制実行の場合、ワイプとタイミングが重ならないように少し待ってから発火
+            time += Time.deltaTime;
+            if (time >= delayTime)
+            {
+                talkScript.SetScenario(scenarioFileName, false);
+                Destroy(gameObject);
+            }
+        }
     }
 
     public void OnTriggerStay2D(Collider2D collision)
